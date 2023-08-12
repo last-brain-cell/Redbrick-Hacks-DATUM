90197c0ab0bea2bd59ccf91472f0b704bc936f26
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-08-12 17:53:03 +0530

    api template with placeholders for data Naad

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,24 +1,61 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Set up CORS
+origins = ["http://localhost", "http://localhost:8080", "http://localhost:63343"]  # Add your frontend URLs here
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # In-memory database for patient profiles
 profile_db = {
     'patient_name': "John Doe",
-    'Age': 60,
+    'age': 60,
     'diagnosis': "Alzheimer’s disease",
+    "profile_picture": "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+    "patient_address": "Delhi, India",
+    'notes': ""
 }
 
-# Route to get patient profile by ID
-@app.get("/patient")
+patient_data = []
+
+# Route to get patient profile
+@app.get("/patient/profile")
 def get_patient_profile():
     return profile_db
 
 # Route to update patient parameters
-@app.put("/patient")
+@app.put("/patient/profile")
 def update_patient_params(patient_updates: dict):
     profile_db.update(patient_updates)
     return {"message": "Patient parameters updated"}
+
+@app.get("/patient/location")
+def get_patient_location():
+    return {
+        'message': "Work in progress",
+        "must contain": "patient location"
+    }
+
+@app.get("/patient/vitals")
+def get_patient_vitals():
+    return {
+        "message": "Work in progress",
+        "must contain": "Glucometer, heart rate tracker, GPS tracker, BP monitoring, SPO2"
+    }
+
+@app.get("/patient/ehr")
+def get_patient_ehr():
+    return {
+        "message": "Work in progress",
+        "must contain": "EHR DATA"
+    }
 
 
 # Run the server with Uvicorn
